c938a2ee3959359894ebdc2734576d80c8f46251
vicdevcode <vicdevcode@gmail.com>
2024-05-21 17:07:06 +0900

    fix/chat

--- chat/prisma/migrations/20240521080656_chat/migration.sql
+++ chat/prisma/migrations/20240521080656_chat/migration.sql
@@ -1,3 +1,6 @@
+-- CreateEnum
+CREATE TYPE "UserType" AS ENUM ('ADMIN', 'STUDENT', 'TEACHER', 'EMPLOYEE', 'MODERATOR');
+
 -- CreateTable
 CREATE TABLE "Faculty" (
     "id" TEXT NOT NULL,
@@ -7,10 +10,19 @@ CREATE TABLE "Faculty" (
 );
 
 -- CreateTable
-CREATE TABLE "Group" (
+CREATE TABLE "Department" (
     "id" TEXT NOT NULL,
     "name" TEXT NOT NULL,
     "facultyId" TEXT NOT NULL,
+
+    CONSTRAINT "Department_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Group" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "departmentId" TEXT NOT NULL,
     "curatorId" TEXT,
 
     CONSTRAINT "Group_pkey" PRIMARY KEY ("id")
@@ -23,6 +35,7 @@ CREATE TABLE "User" (
     "surname" TEXT NOT NULL,
     "middlename" TEXT,
     "email" TEXT NOT NULL,
+    "roleType" "UserType" NOT NULL,
 
     CONSTRAINT "User_pkey" PRIMARY KEY ("id")
 );
@@ -44,8 +57,25 @@ CREATE TABLE "Teacher" (
     CONSTRAINT "Teacher_pkey" PRIMARY KEY ("id")
 );
 
+-- CreateTable
+CREATE TABLE "Employee" (
+    "id" TEXT NOT NULL,
+    "userId" TEXT NOT NULL,
+
+    CONSTRAINT "Employee_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "_DepartmentToEmployee" (
+    "A" TEXT NOT NULL,
+    "B" TEXT NOT NULL
+);
+
 -- CreateIndex
 CREATE UNIQUE INDEX "Faculty_name_key" ON "Faculty"("name");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Department_name_key" ON "Department"("name");
 
 -- CreateIndex
 CREATE UNIQUE INDEX "Group_name_key" ON "Group"("name");
@@ -59,8 +89,20 @@ CREATE UNIQUE INDEX "Student_userId_key" ON "Student"("userId");
 -- CreateIndex
 CREATE UNIQUE INDEX "Teacher_userId_key" ON "Teacher"("userId");
 
+-- CreateIndex
+CREATE UNIQUE INDEX "Employee_userId_key" ON "Employee"("userId");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "_DepartmentToEmployee_AB_unique" ON "_DepartmentToEmployee"("A", "B");
+
+-- CreateIndex
+CREATE INDEX "_DepartmentToEmployee_B_index" ON "_DepartmentToEmployee"("B");
+
 -- AddForeignKey
-ALTER TABLE "Group" ADD CONSTRAINT "Group_facultyId_fkey" FOREIGN KEY ("facultyId") REFERENCES "Faculty"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE "Department" ADD CONSTRAINT "Department_facultyId_fkey" FOREIGN KEY ("facultyId") REFERENCES "Faculty"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Group" ADD CONSTRAINT "Group_departmentId_fkey" FOREIGN KEY ("departmentId") REFERENCES "Department"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
 ALTER TABLE "Group" ADD CONSTRAINT "Group_curatorId_fkey" FOREIGN KEY ("curatorId") REFERENCES "Teacher"("id") ON DELETE SET NULL ON UPDATE CASCADE;
@@ -73,3 +115,12 @@ ALTER TABLE "Student" ADD CONSTRAINT "Student_groupId_fkey" FOREIGN KEY ("groupI
 
 -- AddForeignKey
 ALTER TABLE "Teacher" ADD CONSTRAINT "Teacher_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Employee" ADD CONSTRAINT "Employee_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "_DepartmentToEmployee" ADD CONSTRAINT "_DepartmentToEmployee_A_fkey" FOREIGN KEY ("A") REFERENCES "Department"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "_DepartmentToEmployee" ADD CONSTRAINT "_DepartmentToEmployee_B_fkey" FOREIGN KEY ("B") REFERENCES "Employee"("id") ON DELETE CASCADE ON UPDATE CASCADE;
